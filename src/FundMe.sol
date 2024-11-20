017f90e639046ae79e6f68482619ecd442f2711c
Harsh Raj <146359469+harshraj070@users.noreply.github.com>
2024-11-20 20:05:20 +0530

    Update FundMe.sol

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -25,26 +25,22 @@ contract FundMe {
 
     function fund() public payable {
         require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
-        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         s_addressToAmountFunded[msg.sender] += msg.value;
         s_funders.push(msg.sender);
     }
 
     function getVersion() public view returns (uint256) {
-        //AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        //return priceFeed.version();
         return s_priceFeed.version();
     }
 
     modifier onlyOwner() {
-        // require(msg.sender == owner);
         if (msg.sender != i_owner) revert FundMe__NotOwner();
         _;
     }
 
-    function cheaperWithdraw() public onlyOwner{
-        uint256 fundersLength = s_funders.length; //reading length from memory not storage
-        for (uint256 funderIndex = 0; funderIndex < fundersLength; funderIndex++){
+    function cheaperWithdraw() public onlyOwner {
+        uint256 fundersLength = s_funders.length;
+        for (uint256 funderIndex = 0; funderIndex < fundersLength; funderIndex++) {
             address funder = s_funders[funderIndex];
             s_addressToAmountFunded[funder] = 0;
         }
@@ -55,29 +51,9 @@ contract FundMe {
             address funder = s_funders[funderIndex];
             s_addressToAmountFunded[funder] = 0;
         }
-        s_funders = new address[](0);
-        // // transfer
-        // payable(msg.sender).transfer(address(this).balance);
-
-        // // send
-        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        // require(sendSuccess, "Send failed");
-
-        // call
-        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        s_funders = new address       (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
     }
-    // Explainer from: https://solidity-by-example.org/fallback/
-    // Ether is sent to contract
-    //      is msg.data empty?
-    //          /   \
-    //         yes  no
-    //         /     \
-    //    receive()?  fallback()
-    //     /   \
-    //   yes   no
-    //  /        \
-    //receive()  fallback()
 
     fallback() external payable {
         fund();
@@ -88,28 +64,23 @@ contract FundMe {
     }
 
     /**
-    * view / pure functions (getters) in order to test the updation of variables
+    * View / pure functions (getters) in order to test the updation of variables
     */
-   function getAddressToAmountFunded(
-    address fundingAddress
-   ) external view returns(uint256){
-    return s_addressToAmountFunded[fundingAddress];
-   }
+    function getAddressToAmountFunded(
+        address fundingAddress
+    ) external view returns (uint256) {
+        return s_addressToAmountFunded[fundingAddress];
+    }
 
-   function getFunder(uint256 index) external view returns (address){
-    return s_funders[index];
-   }
+    function getFunder(uint256 index) external view returns (address) {
+        return s_funders[index];
+    }
 
-   function getOwner() external view returns (address){
-    return i_owner;
-   }
+    function getOwner() external view returns (address) {
+        return i_owner;
+    }
+
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
 }
-
-// Concepts we didn't cover yet (will cover in later sections)
-// 1. Enum
-// 2. Events
-// 3. Try / Catch
-// 4. Function Selector
-// 5. abi.encode / decode
-// 6. Hash with keccak256
-// 7. Yul / Assembly
